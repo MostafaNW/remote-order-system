c2c41aa9859ba6a82fd0ef5406e48c9667823491
MostafNW <moso@DESKTOP-0AHVU0S.localdomain>
2021-09-20 00:08:11 -0400

    basic node shell

--- src/backend/routes/menu.js
+++ src/backend/routes/menu.js
@@ -1,15 +1,25 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 // pools will use environment variables
 // for connection information
 //database
 const dotenv = require("dotenv").config(); //credentials for database
-const { Pool, Client } = require('pg')
-const pool = new Pool()
+const { Pool, Client } = require("pg");
+const pool = new Pool();
+
 /* GET users listing. */
-router.get('/:id', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/:id", async (req, res, next) => {
   console.log(req.params.id);
+  const query =
+    "SELECT id, title, content FROM restaurant.menu AS menu WHERE menu.id = $1";
+  const values = [req.params.id];
+  try {
+    const result = await pool.query(query, values);
+    res.status(200).json(result.rows[0]);
+  } catch (err) {
+    console.log(`Error: ${err}`);
+    res.status(500).json({ message: "Error" });
+  }
 });
 
 module.exports = router;
--- src/sql/dummy_data.sql
+++ src/sql/dummy_data.sql
@@ -1,2 +1,4 @@
 insert into restaurant.meal_item(name,price,content) values('tiger roll',3.49, 'this is healthy');
 insert into restaurant.meal_item(name,price,content) values('poutine',5.58, 'this is may not be healthy');
+--menus
+insert into restaurant.menu(title,content) values('Sushi','Delicious sushi');
