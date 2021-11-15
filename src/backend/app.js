dbb2ac1c61a56072e38f21281c131495bb433956
MostafaNW <moso@LAPTOP-DKM4JVD4.localdomain>
2021-11-15 16:01:23 -0500

    cleaned up some code

--- src/backend/app.js
+++ src/backend/app.js
@@ -25,6 +25,15 @@ app.set("views", path.join(__dirname, "views"));
 // app.use("/api/", indexRouter);
 app.use("/api/menus", menusRouter);
 app.use("/api/items", itemsRouter);
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.status(500).json("ERROR");
+});
 //setting up handlebars view engine
 app.engine(
   "handlebars",
@@ -33,15 +42,5 @@ app.engine(
   })
 );
 app.set("view engine", "handlebars");
-
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
 
 module.exports = app;
